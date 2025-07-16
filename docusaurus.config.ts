b5b81cc6f92e26493298d7b2458a807fdc025120
Micah Lee <micah@micahflee.com>
2025-07-15 17:44:59 -0700

    Disable dark mode

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -49,6 +49,11 @@ const config: Config = {
 
   themeConfig: {
     image: "img/docusaurus-social-card.jpg",
+    colorMode: {
+      defaultMode: "light",
+      disableSwitch: true,
+      respectPrefersColorScheme: false,
+    },
     navbar: {
       logo: {
         alt: "Cyd Logo",
