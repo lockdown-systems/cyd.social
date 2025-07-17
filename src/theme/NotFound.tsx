e926c8b92c55f0e063d2235bc5d684836fdd9f18
Micah Lee <micah@micahflee.com>
2025-07-16 18:39:29 -0700

    Add custom 404 page

---
+++ src/theme/NotFound.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Layout from "@theme/Layout";
+import styles from "../pages/404.module.css";
+
+export default function NotFound() {
+  return (
+    <Layout
+      title="Page Not Found"
+      description="The page you are looking for could not be found."
+    >
+      <main className={styles.mainSection}>
+        <div className={styles.container}>
+          <div className={styles.content}>
+            <h1 className={styles.title}>Page Not Found</h1>
+            <img
+              src="/img/omgkevin2.svg"
+              alt="Page Not Found"
+              className={styles.image}
+            />
+            <div className={styles.buttonContainer}>
+              <a href="/" className="buttonPrimary">
+                Go to Homepage
+              </a>
+              <a href="/download/" className="buttonSecondary buttonSmall">
+                Download Cyd
+              </a>
+            </div>
+          </div>
+        </div>
+      </main>
+    </Layout>
+  );
+}
